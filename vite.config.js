ca105e29eaf7364814402a12c3510d3e7aed593a
oswhyteknits <orincywhyte@gmail.com>
2022-07-11 14:31:23 -0400

    last

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
-    assetsDir: "build",
-    publicDir: "assets",
+    assetsDir: "assets",
+    publicDir: "public",
   },
 });
