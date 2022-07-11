759835c698ebd7ee78261431e46a97e63726d54d
oswhyteknits <orincywhyte@gmail.com>
2022-07-11 14:00:45 -0400

    changed folder

--- src/data.js
+++ src/data.js
@@ -3,28 +3,28 @@ export default {
     grid: [
       {
         id: "1",
-        img: "./src/images/image5.jpg",
+        img: "./src/assets/image5.jpg",
         header: "Solutions",
         text: "Design through critical thinking and problem solving.",
         color: "#D68A56",
       },
       {
         id: "2",
-        img: "./src/images/image9.jpg",
+        img: "./src/assets/image9.jpg",
         header: "Narrative",
         text: "Thoughtfully crafting brands that tell meaningful stories.",
         color: "#F9B3AB",
       },
       {
         id: "3",
-        img: "./src/images/image6.jpg",
+        img: "./src/assets/image6.jpg",
         header: "Bespoke",
         text: "Personalizing our design process for each of our clients.",
         color: "#ECAC5B",
       },
       {
         id: "4",
-        img: "./src/images/image10.jpg",
+        img: "./src/assets/image10.jpg",
         header: "Planet",
         text: "Focusing on delivering environmentally friendly solutions.",
         color: "#526749",
@@ -32,18 +32,18 @@ export default {
     ],
 
     images: [
-      "../src/images/image1.jpg",
-      "../src/images/image2.jpg",
-      "../src/images/image3.jpg",
-      "../src/images/image4.jpg",
-      "../src/images/image5.jpg",
-      "../src/images/image6.jpg",
-      "../src/images/image7.jpg",
-      "../src/images/image8.jpg",
-      "../src/images/image9.jpg",
-      "../src/images/image10.jpg",
+      "./src/assets/image2.jpg",
+      "./src/assets/image3.jpg",
+      "./src/assets/image4.jpg",
+      "./src/assets/image1.jpg",
+      "./src/assets/image5.jpg",
+      "./src/assets/image6.jpg",
+      "./src/assets/image7.jpg",
+      "./src/assets/image8.jpg",
+      "./src/assets/image9.jpg",
+      "./src/assets/image10.jpg",
     ],
-    
+
     altTxt: [
       "Candle posed in front of backdrop",
       "Bottles laid flat",
