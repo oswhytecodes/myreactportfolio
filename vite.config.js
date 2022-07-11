d0bdda62c2d14d403bda0f9d8e00cf3298f04b9f
oswhyteknits <orincywhyte@gmail.com>
2022-07-11 15:17:20 -0400

    kj

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
-    assetsDir: "public",
+    
     publicDir: "public",
   },
 });
